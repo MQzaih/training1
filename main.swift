729ee94d947b5da3652f10eaf64316cafe9f449e
MQzaih <masaqzaih@gmail.com>
2020-09-22 10:53:58 +0300

    stack added

--- main.swift
+++ main.swift
@@ -10,3 +10,21 @@ import Foundation
 
 print("Hello, World!")
 
+
+var employeesOfCompany = Stack <Node>()
+print(employeesOfCompany.valuesOfStack)
+
+var employee1 = Node(titleOfJob:"Director")
+employeesOfCompany.push(value: employee1)
+var employee2 = Node(titleOfJob:"CEO")
+employeesOfCompany.push(value: employee2)
+var employee3 = Node(titleOfJob:"Marketing")
+employeesOfCompany.push(value: employee3)
+
+print(employeesOfCompany.valuesOfStack.count)
+while !(employeesOfCompany.isEmpty()){
+    let testing = employeesOfCompany.peek()!
+    print(testing.description!)
+    employeesOfCompany.pop()
+
+}
