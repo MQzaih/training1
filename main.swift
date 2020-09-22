2d3d50617ce00204101033afeb0e10d88a6a7356
MQzaih <masaqzaih@gmail.com>
2020-09-22 10:36:02 +0300

    Intial Commit

---
+++ main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Queues and Stacks
+//
+//  Created by Asal 2 on 22/09/2020.
+//  Copyright © 2020 Asal 2. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
